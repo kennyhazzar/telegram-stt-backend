d27e2aea64477a429f91aca9a0132b63874ebca4
Sergey Kiselev <98kiselev@gmail.com>
2024-08-28 21:15:32 +0700

    Add entities

---
+++ src/resources/balance/entities/balance.entity.ts
@@ -0,0 +1,12 @@
+import { Entity, Column, ManyToOne } from 'typeorm';
+import { PrimaryUuidBaseEntity } from '@core/db';
+import { User } from '@resources/users/entities/user.entity';
+
+@Entity('balance')
+export class Balance extends PrimaryUuidBaseEntity {
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  amount: number;
+
+  @ManyToOne(() => User, (user) => user.balance)
+  user: User;
+}
---
+++ src/resources/users/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, Column, DeleteDateColumn, OneToMany, Unique } from 'typeorm';
+import { PrimaryUuidBaseEntity } from '@core/db';
+import { Balance } from '@resources/balance/entities/balance.entity';
+import { Task } from '@resources/tasks/entities/task.entity';
+
+@Entity('users')
+export class User extends PrimaryUuidBaseEntity {
+  @Column({ type: 'bigint', unique: true })
+  telegramId: number;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  username: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  firstName: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  secondName: string;
+
+  @Column({ type: 'varchar', length: 32, unique: true })
+  md5: string;
+
+  @OneToMany(() => Balance, (balance) => balance.user)
+  balance: Balance[];
+
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true })
+  deletedAt: Date | null;
+
+  @OneToMany(() => Task, (task) => task.user)
+  task: Task[];
+}
