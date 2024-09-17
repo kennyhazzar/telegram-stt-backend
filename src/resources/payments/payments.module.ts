6500a1b98464cb45af5469b8837ced0efd78dea6
kennyhazzar <dmitriventro@gmail.com>
2024-09-17 10:30:50 +0700

    fix dependencies

--- src/resources/payments/payments.module.ts
+++ src/resources/payments/payments.module.ts
@@ -5,11 +5,12 @@ import { HttpModule } from '@nestjs/axios';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment, PaymentStatus } from './entities';
 import { CacheModule } from '@nestjs/cache-manager';
-import { CacheConfig } from '@core/configs';
+import { CacheConfig, JwtConfig } from '@core/configs';
 import { RedisClientOptions } from 'redis';
 import { EntityService } from '@core/services';
 import { BalanceModule } from '../balance/balance.module';
 import { UserModule } from '../user';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { UserModule } from '../user';
     HttpModule,
     TypeOrmModule.forFeature([Payment, PaymentStatus]),
     CacheModule.registerAsync<RedisClientOptions>(CacheConfig),
+    JwtModule.registerAsync(JwtConfig),
   ],
   controllers: [PaymentsController],
   providers: [PaymentsService, EntityService],
