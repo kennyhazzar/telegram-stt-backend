990a85b1efb8be7b7f8b539155fae4bd4f562ea2
kennyhazzar <dmitriventro@gmail.com>
2024-09-30 15:41:48 +0700

    update http status code for /tg-webapp

--- src/resources/auth/auth.controller.ts
+++ src/resources/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { TelegramDataDto } from './dto';
 import { ThrottlerBehindProxyGuard } from './guards';
@@ -9,6 +9,7 @@ export class AuthController {
 
   @Post('tg-webapp')
   @UseGuards(ThrottlerBehindProxyGuard)
+  @HttpCode(HttpStatus.OK)
   loginViaTelegram(@Body() { data }: TelegramDataDto) {
     return this.authService.loginBySecret(data);
   }
